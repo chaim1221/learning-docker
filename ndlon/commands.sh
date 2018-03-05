2014c650bcbf79a88e53c3cf9dbe142fa7acd9e2
Chaim Eliyah <chaimeliyah@gmail.com>
2018-03-04 22:09:34 -0800

    humble beginnings

--- ndlon/commands.sh
+++ ndlon/commands.sh
@@ -3,4 +3,5 @@ if [ $images -lt 1 ]; then
   docker pull mysql
 fi
 
-docker run mysql # errors because missing args
+# docker run mysql # errors because missing args
+docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql
